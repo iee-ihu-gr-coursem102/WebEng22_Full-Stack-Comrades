ac0769b962aea7e1c86fe556179f94d6b8ec96d6
nikolasTsakiris <nikolasTsakiris@protonmail.com>
2022-12-01 12:27:56 +0200

    Feature: added signup functionality.

--- auth.py
+++ auth.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+import sqlite3 as sql
+from . import database
+from . import db_controller
 
 auth = Blueprint('auth', __name__)
 
@@ -9,3 +14,23 @@ def login():
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    mail = request.form.get('mail')
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(mail=mail).first() 
+
+    if user:
+        return redirect(url_for('auth.signup'))
+
+    new_user = User(mail = mail, username = username, password = generate_password_hash(password, method='sha256'))
+
+    database.session.add(new_user)
+    database.session.commit()
+
+    db_controller.insert_user(mail, username, password)
+
+    return redirect(url_for('auth.login'))
