f2f9e23b9b57287c58b32172349adfea66a4a4fb
nikolasTsakiris <nikolasTsakiris@protonmail.com>
2022-12-17 15:46:36 +0200

    Feature: updated presentation for user preferences.

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from flask import render_template, request
 from flask import Blueprint
 import pandas as pd
 from flask_login import login_required, current_user
+from . import db_controller
 from . import fetching as ftc
 
 main = Blueprint('main', __name__)
 
 @main.route('/', methods = ['GET', 'POST'])
 def index():
+    print(f'user now is: {current_user.username}')
+    db_controller.insert_preference(current_user.username)
     cols_bs_d: list = ['year','department','baseLast']
     cols_bs_ds_y: list = ['departments','baseLast']
     df_bs_d: pd.core.frame.DataFrame  = ftc.to_dataFrame(ftc.get_bases_department(), cols_bs_d)
